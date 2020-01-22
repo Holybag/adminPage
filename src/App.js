32b569a839514d9378612ab7d4f720a99ac1ad44
yhkim <holybag@gmail.com>
2020-01-23 00:37:21 +0100

    User Add/Edit/Delete finished

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
 
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-//import { Nav, NavItem, NavLink } from 'reactstrap';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Nav, NavItem, NavLink } from 'reactstrap';
 
 import { User, Contents, ChatContents } from './pages';
 
@@ -11,12 +11,24 @@ import { User, Contents, ChatContents } from './pages';
 function App() {
   return (
     <Router>
-      <nav class="nav flex-column">
-        <a class="nav-link" href="/user">User</a>
-        <a class="nav-link" href="/Contents">Contents</a>
-        <a class="nav-link" href="#">Chat Rooms</a>
-        <a class="nav-link" href="/ChatContents">Chat Contents</a>
-      </nav>
+    {/* //   <nav class="nav flex-column">
+    //     <a class="nav-link" href="/user">User</a>
+    //     <a class="nav-link" href="/Contents">Contents</a>
+    //     <a class="nav-link" href="#">Chat Rooms</a>
+    //     <a class="nav-link" href="/ChatContents">Chat Contents</a>
+    //   </nav> */}
+
+      <Nav vertical>
+        <NavItem>
+          <NavLink href="/user">User</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="/contents">Contents</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="/chatcontents">Chat Contents</NavLink>
+        </NavItem>
+      </Nav>
 
       <Route exact path='/user' component={User}/>
       <Route path='/contents' component={Contents}/>
--- src/pages/User.js
+++ src/pages/User.js
@@ -1,25 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 import axios from 'axios';
 import { Table, Button } from 'reactstrap';
 import { Modal, ModalHeader, ModalBody, ModalFooter} from 'reactstrap';
-import { Form, FormGroup, Label, Col, Input } from 'reactstrap';
+import { Form, FormGroup, Label, Col, Input, Alert } from 'reactstrap';
 // import UserAddPopup from './UserAddPopup';
 
 const API_URL = 'http://localhost:5000';
 
-class User extends Component {
+class User extends React.Component {
     constructor(props) {
         super(props);
         this.state= {
             users: [],
-            addModalShow: false
+            modalShow: false,
+            modalTitle: '',
+            curId: '',
+            curEmail: '',
+            curPassword: '',
+            curPasswordConfirm: '',
+            curName: '',
+            curCreatedAt: '',
+            curWarningMessage: '',
         }
-        this.showModal = this.showModal.bind(this);
-        this.hideModal = this.hideModal.bind(this);
-        this.isOpen = this.isOpen.bind(this);
     }
 
     componentDidMount() {
+        this.loadUsers();
+    }
+    
+    loadUsers = () => {
         const url = `${API_URL}/users/`;
         axios.get(url).then(response => response.data)
         .then((data) => {
@@ -28,22 +37,118 @@ class User extends Component {
         })
     }
     
+    handleModal = (id) => {
+        console.log('handleModal');
+        console.log('modalShow1:' + this.state.modalShow);
+        this.setState(prevState => ({
+            modalShow: !prevState.modalShow
+          }));
+        if (!id) {
+            this.setState({
+                modalTitle: 'Add User',
+                curId: '',
+                curEmail: '',
+                curName: '',
+                curPassword: '',
+                curPasswordConfirm: '',
+                curCreatedAt: '',
+            });
+        } else {
+            for (const user of this.state.users){
+                console.log('id:' + user.id);
+                if (user.id === id){
+                    this.setState({
+                        modalTitle: 'Edit User',
+                        curId: user.id,
+                        curEmail: user.email,
+                        curName: user.name,
+                        curPassword: user.password,
+                        curPasswordConfirm: user.password,
+                        curCreatedAt: user.created_at
+                    });        
+                    break;
+                }
+            }
+        }
+        console.log('modalShow2:' + this.state.modalShow);
+    }
+
+    handleChange = (e) => {
+        console.log(e.target.name);
+        console.log(e.target.value);
+        this.setState({ [e.target.name]: e.target.value });
+    }    
+
+    addEditUser = (id) => {
+        
+        if (!this.state.curEmail) {
+            this.setState({
+                curWarningMessage: 'Please input the Email.'
+            });
+            return;
+        }
+        if (!this.state.curName){
+            this.setState({
+                curWarningMessage: 'Please input the Name.'
+            });
+            return;
+        }
+        if (!this.state.curPassword){
+            this.setState({
+                curWarningMessage: 'Please input the Password.'
+            });
+            return;
+        }
+        if (this.state.curPassword !== this.state.curPasswordConfirm){
+            this.setState({
+                curWarningMessage: 'Please check the Password and Password Confirm.'
+            });
+            return;
+        }
+
+        if (this.state.modalTitle === 'Add User'){
+            const url = `${API_URL}/users/`;
+            axios.post(url, {
+                email: this.state.curEmail,
+                password: this.state.curPassword,
+                name: this.state.curName
+            }).then(response => {
+                this.toggle();
+                this.loadUsers();
+            });
+        } else {
+            const url = `${API_URL}/users/${id}`;
+            axios.put(url, {
+                email: this.state.curEmail,
+                password: this.state.curPassword,
+                name: this.state.curName
+            }).then(response => {
+                this.toggle();
+                this.loadUsers();
+            });
+        }
+
+        
+    }
     
-    showModal = () => {
-        //setIsOpen(true);
+    delUser = (id) => {
+        const url = `${API_URL}/users/${id}`;
+        axios.delete(url, {
+            
+        }).then(response => {
+            this.toggle();
+            this.loadUsers();
+        });
     }
 
-    hideModal = () => {
-        //setIsOpen(false);
-    }
-
-    isOpen() {
-
+    toggle = () => {
+        this.setState(prevState => ({
+            modalShow: !prevState,
+            curWarningMessage: ''
+        }));
     }
 
     render() {
-        let addModalClose =() => this.setState({addModalShow: false});
-
         return (
             <div>
                 <h3>User Management</h3>
@@ -60,64 +165,67 @@ class User extends Component {
                     </thead>
                     <tbody>
                         {this.state.users.map((user) => (
-                            <tr>
+                            <tr key={user.id}>
                                 <td>1</td>
                                 <td>{user.name}</td>
                                 <td>{user.email}</td>
                                 <td>{user.created_at}</td>
-                                <th><Button>Edit</Button><Button>Del</Button></th>
+                                <th>
+                                    <Button onClick={ () => this.handleModal(user.id) }>Edit</Button>
+                                    <Button onClick={ () => this.delUser(user.id) }>Del</Button>
+                                </th>
                             </tr>
                         ))}                        
                     </tbody>
                 </Table>
-                <Button onClick={()=> this.setState({addModalShow: true})}>Add User</Button>
-                {/* <UserAddPopup
-                    show={this.state.addModalShow}
-                    onHide={addModalClose}
-                /> */}
-                <Modal isOpen={this.state.addModalShow} fade={false} 
+                <Button onClick={ ()=> this.handleModal() }>Add User</Button>
                 
-                size="lg"
-                aria-labelledby="contained-modal-title-vcenter"
-                centered
+                <Modal isOpen={this.state.modalShow} toggle={ this.toggle } fade={false} className={this.props.className}                
+                    size="lg"
+                    aria-labelledby="contained-modal-title-vcenter"
+                    centered
                 >
-                    <ModalHeader closeButton>
-                        {/* <Modal.Title id="contained-modal-title-vcenter">
-                            User Add
-                        </Modal.Title> */}
+                    <ModalHeader toggle={this.toggle}>
+                        {this.state.modalTitle}
                     </ModalHeader>
                     <ModalBody>
                         <Form>
                             <FormGroup row>
                                 <Label for="email" sm={2}>Email</Label>
                                 <Col sm={10}>
-                                    <Input type="email" name="email" id="email" placeholder="Email address" />
+                                    <Input type="email" name="curEmail" id="email" value={this.state.curEmail} onChange={this.handleChange} placeholder="Email address"/>
                                 </Col>
                             </FormGroup>
                             <FormGroup row>
                                 <Label for="name" sm={2}>Name</Label>
                                 <Col sm={10}>
-                                    <Input type="text" name="name" id="name" placeholder="Name" />
+                                    <Input type="text" name="curName" id="name" value={this.state.curName} onChange={this.handleChange} placeholder="Name" />
                                 </Col>
                             </FormGroup>
                             <FormGroup row>
                                 <Label for="password" sm={2}>Password</Label>
                                 <Col sm={10}>
-                                    <Input type="password" name="password" id="password" placeholder="Password" />
+                                    <Input type="password" name="curPassword" id="password" value={this.state.curPassword} onChange={this.handleChange} placeholder="Password" />
                                 </Col>
                             </FormGroup>
                             <FormGroup row>
                                 <Label for="passwordConfirm" sm={2}>Password Confirm</Label>
                                 <Col sm={10}>
-                                    <Input type="password" name="passwordConfirm" id="passwordConfirm" placeholder="Password Confirm" />
+                                    <Input type="password" name="curPasswordConfirm" id="passwordConfirm" value={this.state.curPasswordConfirm} onChange={this.handleChange} placeholder="Password Confirm" />
                                 </Col>
                             </FormGroup>
-        
-                            <Button>Submit</Button>
+                            <FormGroup row>
+                                <Label for="warningMessage" sm={2}></Label>
+                                <Col sm={10}>
+                                    <Alert color="light" id="warningMessage">{this.state.curWarningMessage}</Alert>
+                                </Col>
+                                
+                            </FormGroup>
                         </Form>
                     </ModalBody>
                     <ModalFooter>
-                        <Button onClick={addModalClose}>Close</Button>
+                        <Button onClick={ () => { this.addEditUser(this.state.curId) } }>{this.state.modalTitle}</Button>
+                        <Button onClick={this.toggle}>Close</Button>
                     </ModalFooter>
                 </Modal>
             </div>
